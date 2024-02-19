836db3e6e2f83bebd144e15d4bb775f8fe48263b
Mayuri <mayurigurav1997@gmail.com>
2024-02-19 15:59:17 +0530

    multiple jsx slots

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,15 +1,16 @@
 import { CORE_CONCEPTS } from "../data";
 import CoreConcept from "./CoreConcept";
+import Section from "./Section";
 
 export default function CoreConcepts(){
     return (
-        <section id="core-concepts">
+        <Section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((conceptItem) => (
               <CoreConcept key={conceptItem.title} {...conceptItem} />
             ))}
           </ul>
-        </section>
+        </Section>
     )
 }
